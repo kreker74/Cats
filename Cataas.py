4b3f5cf45f682155946a6df9192603670af21a58
Karnaukhova.S <karnauhova_74@mail.ru>
2024-09-09 17:11:26 +0300

    Создана функция для получения изображения с котиками

--- Cataas.py
+++ Cataas.py
@@ -3,6 +3,23 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image(url):
+    try:
+        # Отправляем GET-запрос с использованием requests.get()
+        response = requests.get(url)
+        # Проверяем успешность запроса (код ответа 200)
+        response.raise_for_status()
+        # Читаем байты из ответа в объект BytesIO
+        image_data = BytesIO(response.content)
+        # Открываем изображение с помощью PIL
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+        return None
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
